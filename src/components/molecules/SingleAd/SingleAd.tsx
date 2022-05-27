c3f5479670dd5ec840ab676e3401e284442eac65
Artur Majewski <artur.majewski28@gmail.com>
2022-05-27 23:51:47 +0200

    Added popup to map markers with ad information

--- src/components/molecules/SingleAd/SingleAd.tsx
+++ src/components/molecules/SingleAd/SingleAd.tsx
@@ -1,33 +1,35 @@
 import { useEffect, useState } from 'react';
 import { AdEntity } from '../../../../../advertisement-service-back/types/ad';
-import styles from './SingleAd.module.scss'
+import styles from './SingleAd.module.scss';
 
 interface Props {
-  id: string;
+	id: string;
 }
 
-export const SingleAd = ( props: Props) => {
-  const [ad, setAd] = useState<AdEntity | null>(null)
+export const SingleAd = (props: Props) => {
+	const [ad, setAd] = useState<AdEntity | null>(null);
 
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(`http://localhost:3001/ad/${props.id}`)
-      const data = await res.json()
-      setAd(data)
-    }) ()
-  }, [])
+	useEffect(() => {
+		(async () => {
+			const res = await fetch(`http://localhost:3001/ad/${props.id}`);
+			const data = await res.json();
+			setAd(data);
+		})();
+	}, []);
 
-  if(ad === null) {
-    return <span>Loading...</span>
-  }
+	if (ad === null) {
+		return <span>Loading...</span>;
+	}
 
-  return(
-    <>
-    <h2>{ad.name}</h2>
-    <p>{ad.description}</p>
-    {ad.price ?? <p>{ad.price} PLN</p>}
-    <a href={ad.url} target='_blank' rel='noreferrer'>Open</a>
-
-    </>
-  )
-}
+	return (
+		<div className={styles.adWrapper}>
+			<h2 className={styles.adTitle}>{ad.name}</h2>
+			{ad.price ? <p className={styles.adPrice}>Price: {ad.price} PLN</p> : null}
+			<p className={styles.adAddress}>Address: <br/>{ad.address}</p>
+      <p className={styles.adDescription}>Description: <br/>{ad.description}</p>
+			<a href={ad.url} className={styles.adLink} target='_blank' rel='noreferrer'>
+				Open
+			</a>
+		</div>
+	);
+};
