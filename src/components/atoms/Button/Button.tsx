1b792ac5f920caed95a608e37f736a93bf895844
Artur Majewski <artur.majewski28@gmail.com>
2022-05-27 21:41:38 +0200

    Corrected styles in the Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,10 +1,40 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Button.module.scss';
+
+enum sizeStyleName {
+	'small',
+	'medium',
+	'big',
+}
 
 interface Props {
 	name: string;
-  type?: "button" | "submit" | "reset";
+	type?: 'button' | 'submit' | 'reset';
+	to?: string;
+	size?: 'small' | 'medium' | 'big';
 }
 
-export const Button = ({name, type='button'}: Props) => {
-	return <button type={type} className={styles.addBtn}>{name}</button>;
+const sizeStyle = [
+	{ padding: '0 1rem', fontSize: '1rem' },
+	{ padding: '0.75rem 1rem' },
+	{ padding: '1rem' },
+];
+
+export const Button = ({
+	name,
+	type = 'button',
+	to,
+	size = 'medium',
+}: Props) => {
+	const num = sizeStyleName[size];
+
+	return to ? (
+		<NavLink className={styles.addBtn} to={to} style={sizeStyle[num]}>
+			{name}
+		</NavLink>
+	) : (
+		<button type={type} className={styles.addBtn}>
+			{name}
+		</button>
+	);
 };
